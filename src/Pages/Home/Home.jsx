ed5dc34daa79f56d5ca0f00c5f1ecb906f478a94
Hasan <hasan.sherif2005@gmail.com>
2025-11-30 15:52:07 +0200

    reverting last merge

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import TopBar from "../../Components/Shared/TopBar/TopBar";
+
 
 function Home() {
   return (
-    <>
+    <div>
+      <TopBar />
       <h1>Home Page Content</h1>
-    </>
+    </div>
   );
 }
 
