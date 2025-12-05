0305f864e82c3411ac3b2d8121404bff45caca03
Hasan <hasan.sherif2005@gmail.com>
2025-12-06 01:41:22 +0200

    merging hania

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import Home from "./Pages/Home/Home";
-import SearchResults from "./Pages/SearchResults/SearchResults";
 import PostDetails from "./Pages/PostDetails/PostDetails";
-import CreatePost from "./Pages/CreatePost/CreatePost";
 function App() {  
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
